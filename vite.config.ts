49be68d2170c2a84cddc909ec6bc6e7ff18556f0
Mkp991203 <manikumarkuyyeti@gmail.com>
2025-08-09 12:21:33 +0530

    ip

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from 'path';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'), // Alias for 'src'
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from 'path';
@@ -9,5 +23,9 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'), // Alias for 'src'
     },
+  },
+  server: {
+    host: '0.0.0.0', // Allow access from external devices
+    port: 5174,      // Port to run the dev server
   },
 });
